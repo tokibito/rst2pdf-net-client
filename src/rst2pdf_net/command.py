71440d5fe86827decc77a3d9e91de389606940a8
Shinya Okano <tokibito@gmail.com>
2014-05-18 00:01:51 +0900

    コマンドクラス追加

--- src/rst2pdf_net/command.py
+++ src/rst2pdf_net/command.py
@@ -1,2 +1,33 @@
+import sys
+from argparse import ArgumentParser
+
+from . import client
+
+
+class Command(object):
+    def make_argument_parser(self):
+        parser = ArgumentParser()
+        return parser
+
+    def make_client(self):
+        return client.Client()
+
+    def get_input(self, args):
+        # TODO: 引数のファイル指定を見る
+        return sys.stdin
+
+    def get_output(self, args):
+        # TODO: 引数のファイル指定を見る
+        return sys.stdout
+
+    def run(self):
+        parser = self.make_argument_parser()
+        args = parser.parse_args()
+        client = self.make_client()
+        input_stream = self.get_input()
+        output_stream = self.get_output()
+
+
 def main():
-    pass
+    cmd = Command()
+    cmd.run()
